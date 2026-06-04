07844a7c0efba7d5f990e0a13ad9ec2c53d60f2c
KartikReddy1 <skr.somu387@gmail.com>
2026-06-04 22:42:20 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,12 @@
+package com.karthik;
+
+public class Sum {
+    public static void main (String[] args){
+        int a = 80;
+        int b = 40;
+        int sum = a / b;
+
+        System.out.println(sum);
+
+    }
+}
