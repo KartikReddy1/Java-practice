5f1eb11d9b7ef777ee7bf54599b4bb4ebb0b7d58
KartikReddy1 <skr.somu387@gmail.com>
2026-06-06 16:47:50 +0530

    Add files via upload

---
+++ Java practice/Userdata.java
@@ -0,0 +1,18 @@
+package com.java;
+public class Userdata {
+
+    public static void main(String[] args) {
+        String name = "Karthik";
+        String village = "Owk";
+        byte age = 21;
+        int phoneNo = 1234554321;
+
+        System.out.println("name:" +name);
+        System.out.println("phoneNO:" +phoneNo);
+        System.out.println("age:" +age);
+        System.out.println("village:" +village);
+
+
+    }
+
+}
---
+++ Java practice/Variable1.java
@@ -0,0 +1,16 @@
+package com.karthik;
+
+public class Variable1 {
+    public static void main(String[] args) {
+        String name = "Karthik";
+        int age = 20;
+        double height = 6.2;
+        float percent = 70f;
+
+        System.out.println("name:" + name);
+        System.out.println("age:" + age);
+        System.out.println("height:" + height);
+        System.out.println("percent:" + percent);
+
+    }
+}
