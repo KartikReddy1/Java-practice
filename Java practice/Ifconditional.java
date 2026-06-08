1eb0e664ea4b548d512ec74bae26baa353a15c9a
KartikReddy1 <skr.somu387@gmail.com>
2026-06-08 23:12:28 +0530

    Add files via upload

---
+++ Java practice/Ifconditional.java
@@ -0,0 +1,10 @@
+package com.Decisionstatements;
+
+public class Ifconditional {
+    public static void main(String[] args) {
+        int i = -10;
+        if (i<0) {
+            System.out.println("the given number is negative");
+        }
+    }
+}
