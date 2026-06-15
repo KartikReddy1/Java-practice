b0ea3b3ce43d761ff2bd020d73f6d01e648f872a
KartikReddy1 <skr.somu387@gmail.com>
2026-06-15 22:02:06 +0530

    Add files via upload

---
+++ Java practice/Employee.java
@@ -0,0 +1,25 @@
+package com.Methods;
+
+public class Employee {
+    int id;
+    String name;
+    double salary;
+
+    Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    void display() {
+        System.out.println(id);
+        System.out.println(name);
+        System.out.println(salary);
+    }
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee(101, "Ravi", 45000);
+
+        e1.display();
+    }
+}
